d2e37faaf610f739e296ee80cf02b5d3ca5161f3
Gopal Kumar Das <krgopalkumar706@gmail.com>
2024-10-27 13:27:40 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Article, Brand, Cta, Feature, Navbar} from './Components'
+
+const App = () => {
+  return (
+    <>
+      <h1>GPT-3</h1>
+
+    </>
+  )
+}
+
+export default App
